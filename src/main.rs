c55445cd77b99321dcbd25ffeee1b1350fe34d4b
LeadRDRK <leadrdrk@zohomail.com>
2024-12-14 21:26:44 +0700

    v1.1.0

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 mod utils;
 
-use std::{path::{Path, PathBuf}, str::FromStr, sync::mpsc::Sender};
+use std::{path::{Path, PathBuf}, str::FromStr, sync::mpsc::Sender, time::Duration};
 
 use notify::{Result, Watcher};
 use rand::Rng;
@@ -10,19 +10,19 @@ use windows::{core::{w, PCWSTR}, Win32::{Foundation::{CloseHandle, GetLastError,
 
 fn get_dll_paths(game_path: &Path, dll_name: &str) -> (PathBuf, PathBuf) {
     (
-        game_path.join(format!("hachimi\\{}", dll_name)),
-        game_path.join(format!("umamusume_Data\\Plugins\\x86_64\\{}", dll_name))
+        game_path.join(format!("umamusume_Data\\Plugins\\x86_64\\{}", dll_name)),
+        game_path.join(format!("hachimi\\{}", dll_name))
     )
 }
 
 fn create_watcher(
-    mallet_dll: PathBuf, mallet_backup: PathBuf, dest_dll: PathBuf, src_dll: PathBuf, timeout_reset: Sender<()>
+    mallet_dll: PathBuf, mallet_backup: PathBuf, timeout_reset: Sender<()>
 ) -> Result<(notify::ReadDirectoryChangesWatcher, std::thread::JoinHandle<()>)> {
     let (tx, rx) = std::sync::mpsc::channel();
-    let watcher = notify::recommended_watcher(tx)?;
+    let mut watcher = notify::recommended_watcher(tx)?;
+    watcher.watch(&mallet_dll.parent().unwrap(), notify::RecursiveMode::NonRecursive)?;
 
     let mallet_dll_lower = mallet_dll.to_string_lossy().to_ascii_lowercase();
-    let src_dll_lower = src_dll.to_string_lossy().to_ascii_lowercase();
     let handle = std::thread::spawn(move || {
         while let Ok(res) = rx.recv() {
             let Ok(event) = res else { continue };
@@ -31,13 +31,8 @@ fn create_watcher(
 
             if event.kind.is_remove() && path_lower == mallet_dll_lower {
                 _ = timeout_reset.send(());
+                std::thread::sleep(Duration::from_secs(2));
                 _ = std::fs::copy(&mallet_backup, &mallet_dll);
-            }
-            else if event.kind.is_modify() && path_lower == src_dll_lower {
-                _ = timeout_reset.send(());
-                // wait a lil bit so dmm finishes its checks
-                std::thread::sleep(std::time::Duration::from_secs(3));
-                move_target_dll(&dest_dll, &src_dll);
             }
         }
     });
@@ -45,13 +40,14 @@ fn create_watcher(
     Ok((watcher, handle))
 }
 
-fn move_target_dll(dest_dll: &Path, src_dll: &Path) {
+fn move_target_dll(src_dll: &Path, dest_dll: &Path) {
     if std::fs::create_dir_all(dest_dll.parent().unwrap()).is_ok() && std::fs::copy(&src_dll, &dest_dll).is_ok() {
         _ = std::fs::remove_file(&src_dll);
     }
 }
 
-const TIMEOUT_COUNT: u32 = 30;
+const TIMEOUT_COUNT: u32 = 150;
+const TIMEOUT_DURATION: Duration = Duration::from_millis(200);
 fn main() -> Result<()> {
     unsafe {
         let mut exec_path = [0u16; MAX_PATH as usize];
@@ -68,9 +64,9 @@ fn main() -> Result<()> {
 
     let mallet_dll = PathBuf::from_str(&std::env::args().skip(1).next().unwrap()).unwrap();
     let game_path = utils::detect_game_install_dir().unwrap().canonicalize().unwrap();
-    let (dest_dll, src_dll) = get_dll_paths(&game_path, "cri_mana_vpx.dll");
+    let (src_dll, dest_dll) = get_dll_paths(&game_path, "cri_mana_vpx.dll");
 
-    move_target_dll(&dest_dll, &src_dll);
+    move_target_dll(&src_dll, &dest_dll);
 
     let mallet_backup_rnd: String = rand::thread_rng()
         .sample_iter(&rand::distributions::Alphanumeric)
@@ -83,25 +79,28 @@ fn main() -> Result<()> {
     std::fs::copy(&mallet_dll, &mallet_backup).unwrap();
 
     let (tx, rx) = std::sync::mpsc::channel();
-
-    let (mut watcher, handle) = create_watcher(mallet_dll.clone(), mallet_backup.clone(), dest_dll, src_dll.clone(), tx)?;
-    watcher.watch(&src_dll.parent().unwrap(), notify::RecursiveMode::NonRecursive)?;
-    watcher.watch(&mallet_dll.parent().unwrap(), notify::RecursiveMode::NonRecursive)?;
+    let (watcher, handle) = create_watcher(mallet_dll.clone(), mallet_backup.clone(), tx)?;
 
     let mut timeout = TIMEOUT_COUNT;
     while timeout != 0 {
-        if !utils::is_dmm_running() {
-            timeout -= 1;
-        }
-        else if timeout != TIMEOUT_COUNT {
-            timeout = TIMEOUT_COUNT;
+        if let Some(processes) = utils::list_processes() {
+            if !processes.contains(c"DMMGamePlayer.exe") {
+                timeout -= 1;
+            }
+            else if timeout != TIMEOUT_COUNT {
+                timeout = TIMEOUT_COUNT;
+            }
+
+            if processes.contains(c"umamusume.exe") {
+                move_target_dll(&src_dll, &dest_dll);
+            }
         }
 
         if rx.try_recv().is_ok() {
             timeout = TIMEOUT_COUNT;
         }
 
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        std::thread::sleep(TIMEOUT_DURATION);
     }
 
     _ = std::fs::remove_file(&mallet_backup);
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::{ffi::CStr, path::{Path, PathBuf}};
+use std::{collections::HashSet, ffi::{CStr, CString}, path::{Path, PathBuf}};
 
 use tinyjson::JsonValue;
 use windows::Win32::{
@@ -8,24 +8,22 @@ use windows::Win32::{
     UI::Shell::{FOLDERID_RoamingAppData, SHGetKnownFolderPath, KF_FLAG_DEFAULT}
 };
 
-pub fn is_dmm_running() -> bool {
+pub fn list_processes() -> Option<HashSet<CString>> {
     let Ok(snapshot) = (unsafe { CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0) }) else {
-        return false;
+        return None;
     };
     let mut entry = PROCESSENTRY32::default();
     entry.dwSize = std::mem::size_of::<PROCESSENTRY32>() as u32;
     let mut res = unsafe { Process32First(snapshot, &mut entry) };
 
+    let mut set = HashSet::new();
     while res.is_ok() {
         let process_name = unsafe { CStr::from_ptr(entry.szExeFile.as_ptr()) };
-        if process_name == c"DMMGamePlayer.exe" {
-            return true;
-        }
-
+        set.insert(process_name.to_owned());
         res = unsafe { Process32Next(snapshot, &mut entry) };
     }
 
-    false
+    Some(set)
 }
 
 pub fn detect_game_install_dir() -> Option<PathBuf> {
